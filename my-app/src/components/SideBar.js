cc12b5586f4eb20a89da4d7eda9a90a4891612f4
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2022-12-31 02:49:13 -0300

    feat: navbar

--- my-app/src/components/SideBar.js
+++ my-app/src/components/SideBar.js
@@ -2,6 +2,9 @@ import React, { useContext}  from 'react';
 import {Link} from 'react-router-dom';
 import providerFile from '../context/contex';
 import logo from '../components/a.png';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 
 
  function SideBar () {
@@ -23,7 +26,11 @@ return(
   <Link to="/skills"><p>{english ? `My Skills` : `Minhas habilidades`}</p></Link>
   <Link to="/contact"><p>{english ? `Contact` : `contato`}</p></Link>
   <button onClick={()=> setLanguage() } className="langbtn">{english ?  `Portuguese`: `Inglês` }</button>
- 
+ <div className="icons">
+ <a href="https://www.linkedin.com/in/hebertt-nascimento" target="_blank" alt="linkedin" rel="noreferrer" ><LinkedInIcon /></a>
+ <a href="https://github.com/Kamui-Hebertt" target="_blank" alt="github" rel="noreferrer" > <GitHubIcon /></a>  
+  
+ </div>
   </div>
 )
 
