89108fd27effc6d88e67bfb60c7eb93b939ef1a3
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-01-08 16:24:35 -0300

    feat: pt about

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -62,19 +62,34 @@ function Home () {
           <p>About Me</p>
           </div>
           <div className="allAbout">
-            <p className="pAbout">I am obsessed with the Internet and how it works, so experimenting with new technologies is a lot of fun for me. I like to feel challenged and find personal satisfaction in learning new things.
-</p>
+            <p className="pAbout">I am obsessed with the Internet and how it works,
+             so experimenting with new technologies is a lot of fun for me.
+              I like to feel challenged and find personal satisfaction in learning new things.
+                  </p>
           </div>
          </div>
             </div>
             ) : ( 
             <div className="mainContent">
+              <div className="content">
               <img src={ profile } className="profileClass" alt='profile' />
-              <div className='texti'>
+              <div className="texti">
              <p className='hi'>Olá!</p>  
-             <p className='hi2'> Me chamo <img src={H} className="nameLogo" alt="logo" />
+             <p className='hi1'> Me chamo <img src={H} className="nameLogo" alt="logo" />
               ebertt, Desenvolvedor Web  </p>
               </div>
+              </div>
+              <div className='about'>
+           <div className="about1">
+          <p>Sobre mim</p>
+          </div>
+          <div className="allAbout">
+            <p className="pAbout">Sou obcecado pela Internet e como ela funciona,
+             então experimentar novas tecnologias é muito divertido pra mim. Gosto de me sentir desafiada
+              e encontro satisfação pessoal em aprender coisas novas. </p>
+
+          </div>
+         </div>
               
             </div>
             
