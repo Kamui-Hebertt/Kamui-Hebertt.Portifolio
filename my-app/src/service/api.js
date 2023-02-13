bf3510a87d570489c534ea3f38526deda42af184
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-02-13 16:48:37 -0300

    feat: order carosel

--- my-app/src/service/api.js
+++ my-app/src/service/api.js
@@ -4,8 +4,8 @@ export const project = [
     name: 'Trivia',
     descPt: 'Trivia Game',
     photo1: 'https://uploaddeimagens.com.br/images/004/316/576/full/trivia.png?1674863071',
-    photo2: 'https://uploaddeimagens.com.br/images/004/316/576/full/trivia.png?1674863071',
-    photo3: 'https://uploaddeimagens.com.br/images/004/316/576/full/trivia.png?1674863071',
+    photo2: 'https://uploaddeimagens.com.br/images/004/352/507/full/Screenshot_%28231%29.png?1676312376',
+    photo3: 'https://uploaddeimagens.com.br/images/004/352/523/full/Screenshot_%28233%29.png?1676312633',
     url: 'https://trivia-project-redux.vercel.app/',
   },
   {
@@ -13,8 +13,8 @@ export const project = [
     name: 'Recipe App',
     description: '',
     photo1: 'https://uploaddeimagens.com.br/images/004/316/589/full/3ebdbf24-b778-4848-bac5-889519778124.png?1674864175',
-    photo2: 'https://uploaddeimagens.com.br/images/004/316/589/full/3ebdbf24-b778-4848-bac5-889519778124.png?1674864175',
-    photo3: 'https://uploaddeimagens.com.br/images/004/316/589/full/3ebdbf24-b778-4848-bac5-889519778124.png?1674864175',
+    photo2: 'https://uploaddeimagens.com.br/images/004/352/556/full/Screenshot_%28240%29.png?1676313471',
+    photo3: 'https://uploaddeimagens.com.br/images/004/352/564/full/Screenshot_%28239%29.png?1676313811',
     url: 'https://recipe-app-react-jade.vercel.app/',
   },
   {
@@ -22,9 +22,39 @@ export const project = [
     name: 'Trybe Wallet',
     description: '',
     photo1: 'https://uploaddeimagens.com.br/images/004/316/597/full/2935644d-e08d-4cf4-bf81-ba23e7376598.png?1674864981',
-    photo2: 'https://uploaddeimagens.com.br/images/004/316/597/full/2935644d-e08d-4cf4-bf81-ba23e7376598.png?1674864981',
-    photo3: 'https://uploaddeimagens.com.br/images/004/316/597/full/2935644d-e08d-4cf4-bf81-ba23e7376598.png?1674864981',
+    photo2: 'https://uploaddeimagens.com.br/images/004/352/581/full/Screenshot_%28244%29.png?1676314284',
+    photo3: 'https://uploaddeimagens.com.br/images/004/352/582/full/Screenshot_%28243%29.png?1676314467',
     url: 'https://wallet-project-redux.vercel.app/',
-  }
+  },
+
+  {
+    id: 1,
+    name: 'Trivia',
+    descPt: 'Trivia Game',
+    photo1: 'https://uploaddeimagens.com.br/images/004/316/576/full/trivia.png?1674863071',
+    photo2: 'https://uploaddeimagens.com.br/images/004/352/507/full/Screenshot_%28231%29.png?1676312376',
+    photo3: 'https://uploaddeimagens.com.br/images/004/352/523/full/Screenshot_%28233%29.png?1676312633',
+    url: 'https://trivia-project-redux.vercel.app/',
+  },
+  {
+    id: 2,
+    name: 'Recipe App',
+    description: '',
+    photo1: 'https://uploaddeimagens.com.br/images/004/316/589/full/3ebdbf24-b778-4848-bac5-889519778124.png?1674864175',
+    photo2: 'https://uploaddeimagens.com.br/images/004/352/556/full/Screenshot_%28240%29.png?1676313471',
+    photo3: 'https://uploaddeimagens.com.br/images/004/352/564/full/Screenshot_%28239%29.png?1676313811',
+    url: 'https://recipe-app-react-jade.vercel.app/',
+  },
+  {
+    id: 3,
+    name: 'Trybe Wallet',
+    description: '',
+    photo1: 'https://uploaddeimagens.com.br/images/004/316/597/full/2935644d-e08d-4cf4-bf81-ba23e7376598.png?1674864981',
+    photo2: 'https://uploaddeimagens.com.br/images/004/352/581/full/Screenshot_%28244%29.png?1676314284',
+    photo3: 'https://uploaddeimagens.com.br/images/004/352/582/full/Screenshot_%28243%29.png?1676314467',
+    url: 'https://wallet-project-redux.vercel.app/',
+  },
+
+
 
 ];
