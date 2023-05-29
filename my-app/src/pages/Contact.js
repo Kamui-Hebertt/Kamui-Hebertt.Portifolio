61c42f68321821a52b0d1095a9f047e1b834cc97
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-05-29 18:26:25 -0300

    fxing styles

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -1,37 +1,29 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import Footer from '../components/Footer';
-import Loading from '../components/Loading';
-import SideBar from '../components/SideBar';
-import providerFile from '../context/contex';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import Footer from "../components/Footer";
+import Loading from "../components/Loading";
+import SideBar from "../components/SideBar";
+import providerFile from "../context/contex";
+import MenuIcon from "@mui/icons-material/Menu";
 
-import MenuIcon from '@mui/icons-material/Menu'
-
-
-
-
-function Contact () {
-  const { loading, setloading, english, openSideBar } = useContext(providerFile);
+function Contact() {
+  const { loading, setloading, english, openSideBar } =
+    useContext(providerFile);
   const form = useRef();
-  
-   const [formData, setFormData] = useState({
+
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
 
-   useEffect(
-    () => {
-      setloading(true)
-      let timer1 = setTimeout(() => setloading(false), 2000);
-      return () => {
-        clearTimeout(timer1);
-      };
-    },
-    []
-  );
-
-
+  useEffect(() => {
+    setloading(true);
+    let timer1 = setTimeout(() => setloading(false), 2000);
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, []);
 
   const { name, email, message } = formData;
 
@@ -42,15 +34,11 @@ function Contact () {
     });
   };
 
-  
-  
-
   const handleSubmit = (event) => {
     event.preventDefault();
 
-
-    if (!name || !email || !message ) {
-      alert(!english ? 'Preencha todos os campos' : 'fulfil all fields');
+    if (!name || !email || !message) {
+      alert(!english ? "Preencha todos os campos" : "fulfil all fields");
       return;
     }
 
@@ -59,18 +47,23 @@ function Contact () {
       email: email,
       subject: "costumer message",
       description: message,
-      
     };
-    
-    emailjs.send(
-        'service_714sd0j',
-        'template_uuco2rq',
-         templateParams,
-        'jKyrOlP3iZ70djF8G'
+
+    emailjs
+      .send(
+        "service_714sd0j",
+        "template_uuco2rq",
+        templateParams,
+        "jKyrOlP3iZ70djF8G"
       )
       .then(
         (response) => {
-          alert(!english ? 'Mensagem enviada com Sucesso!!' :'Message sent successfully!!', response.text);
+          alert(
+            !english
+              ? "Mensagem enviada com Sucesso!!"
+              : "Message sent successfully!!",
+            response.text
+          );
           setFormData({
             name: "",
             email: "",
@@ -78,7 +71,7 @@ function Contact () {
           });
         },
         (error) => {
-          console.log('error...', error);
+          console.log("error...", error);
         }
       );
 
@@ -93,77 +86,94 @@ function Contact () {
     //     alert("Failed to send email. Please try again later.");
     //   });
   };
-   
 
+  return (
+    <div>
+      <SideBar />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="allContact">
+          <div className="menuBar">
+            <button onClick={openSideBar}>
+              <MenuIcon />
+            </button>
+          </div>
+          <div className="form">
+            <h3>{english ? "How to Contact me" : "Como me contatar"}</h3>
 
-        return(
-            <div  >
-                <SideBar />
-          {loading ? <Loading /> : (
-          <div className="allContact">
-            <div className="menuBar" >
-               <button onClick={openSideBar} >
-               <MenuIcon />
-               </button>
+            <form className="formMain" ref={form} onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="name" className="nameL">
+                  {english ? "Name:" : "Nome:"}
+                </label>
+                <input
+                  className="name"
+                  placeholder={english?"Enter your name" : "Digite seu Nome"}
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div>
+                <label className="emailL" htmlFor="email">
+                  Email:
+                </label>
+                <input
+                  className="email"
+                  placeholder={english?"Enter your email" : "Digite seu email"}
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div>
+                <div className="textfield">
+                  <label className="messageL" htmlFor="message">
+                    {english ? "Message:" : "Mensagem:"}
+                  </label>
+                  <textarea
+                    className="textMessage"
+                    name="message"
+                    value={message}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
-              <div className="form">
-              
-             <h3>{english ? "How to Contact me" : "Como me contatar"}</h3>
-             
-                  <form className="formMain" ref={form} onSubmit={handleSubmit}>
-                    <div>
-                      <label htmlFor="name" className="nameL">{english ?"Name:" : "Nome:"}</label>
-                      <input
-                        className="name"
-                        type="text"
-                        name="name"
-                        value={name}
-                        onChange={handleInputChange}
-                        required />
-                    </div>
-                    <div>
-                      <label className="emailL" htmlFor="email">Email:</label>
-                      <input
-                        className="email"
-                        type="email"
-                        name="email"
-                        value={email}
-                        onChange={handleInputChange}
-                        required />
-                    </div>
-                    <div>
-                      <div className="textfield">
-                      <label className="messageL" htmlFor="message">{english ?"Message:" : "Mensagem:"}</label>
-                      <textarea
-                        className="textMessage"
-                        name="message"
-                        value={message}
-                        onChange={handleInputChange}
-                        required />
-                    </div>
-                    </div>
-                    <button type="submit" className="sendBtn">{english ?"Send" : "Enviar"}</button>
-                  </form>
-                </div>
-                <main className="contact">
-                <div className="textC">
-                  
-                  <p>Email: heberttafetteira.com@gmail.com</p>
-                  <p>{english ? "Phone Number/Whatsapp: +55 2199215-7839" : "Telefone/Whatsapp: +55 2199215-7839"}</p>
+              </div>
+              <button type="submit" className="sendBtn">
+                {english ? "Send" : "Enviar"}
+              </button>
+            </form>
+          </div>
+          <main className="contact">
+            <div className="textC">
+              <p>Email: heberttafetteira.com@gmail.com</p>
+              <p>
+                {english
+                  ? "Phone Number/Whatsapp: +55 2199215-7839"
+                  : "Telefone/Whatsapp: +55 2199215-7839"}
+              </p>
 
-                   <a href="https://www.linkedin.com/in/hebertt-nascimento/" target="_blank" rel="noreferrer"> Linkedin </a>
-                </div>
-
-
-              </main>
-                
-                
-                </div>
-            ) }  
-           {loading ? null : (<Footer />) } 
-            
+              <a
+                href="https://www.linkedin.com/in/hebertt-nascimento/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                Linkedin{" "}
+              </a>
             </div>
-        )
-    }
+          </main>
+        </div>
+      )}
+      {loading ? null : <Footer />}
+    </div>
+  );
+}
 
 export default Contact;
