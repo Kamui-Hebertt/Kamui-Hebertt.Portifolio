8d7a5f02c9496aa04a7eb8d724e11390441d9465
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-06-24 23:18:40 -0300

    feat: starting nav next js

---
+++ my-app/porttifolio/src/app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-black bg-opacity-30 flex h-15">
+   <p className="text-white ml-1">Hebertt Nascimento | Fullstack developer</p>
+
+    </nav>
+  )
+}
